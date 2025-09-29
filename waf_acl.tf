c831dc12408f8f9df926bdad93f89ede15278786
Camus Ulloa-Jonsson <camus@perxhealth.com>
2025-09-29 11:12:14 +1000

    allow rule to be ignored for drift, configure acl via console

--- waf_acl.tf
+++ waf_acl.tf
@@ -113,4 +113,8 @@ resource "aws_wafv2_web_acl" "waf_acl" {
     metric_name                 = "CloudFrontGlobalWafWebAcl"
     sampled_requests_enabled    = true
   }
+
+  lifecycle {
+    ignore_changes = [rule]
+  }
 }
